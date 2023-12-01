640daa7dae3181ea1910930491c1c7d50a79cfb6
UdayKhare09 <udaykhare77@gmail.com>
2023-12-01 21:17:26 +0530

    Random Stuff

--- src/Maths/Matrix.java
+++ src/Maths/Matrix.java
@@ -17,7 +17,7 @@ public class Matrix {
             System.out.println("Choose an operation");
             System.out.println("1. Matrix A");
             System.out.println("2. Matrix B");
-            System.out.println("3. Multiply");
+            System.out.println("3. Multiply A and B");
             System.out.println("4. Inverse of Matrix A");
             System.out.println("5. Back");
             Scanner scanner = new Scanner(System.in);
@@ -27,7 +27,8 @@ public class Matrix {
                     System.out.println("1. Edit Matrix A");
                     System.out.println("2. View Matrix A");
                     System.out.println("3. Determinant");
-                    System.out.println("4. Back");
+                    System.out.println("4. Square");
+                    System.out.println("5. Back");
                     choice = scanner.nextInt();
 
                     if (choice == 1) {
@@ -41,10 +42,13 @@ public class Matrix {
                         }
                     } else if (choice == 3) {
                         System.out.println(determinant(matA));
-                    } else if (choice == 4) {
-                        matrix();
                     } else if (choice == 5) {
-                        MainMenu.mainmenu();
+                        matrix();
+                    } else if (choice == 4) {
+                        multiply(matA, matA);
+                        System.out.printf("|%d %d %d|\n" +
+                                "|%d %d %d|\n" +
+                                "|%d %d %d|\n", matM[0][0], matM[0][1], matM[0][2], matM[1][0], matM[1][1], matM[1][2], matM[2][0], matM[2][1], matM[2][2]);
                     } else {
                         System.out.println("Invalid choice");
                     }
@@ -54,7 +58,8 @@ public class Matrix {
                     System.out.println("1. Edit Matrix B");
                     System.out.println("2. View Matrix B");
                     System.out.println("3. Determinant");
-                    System.out.println("4. Back");
+                    System.out.println("4. Square");
+                    System.out.println("5. Back");
                     choice = scanner.nextInt();
 
                     if (choice == 1) {
@@ -68,14 +73,19 @@ public class Matrix {
                         }
                     } else if (choice == 3) {
                         System.out.println(determinant(matB));
-                    } else if (choice == 4) {
+                    } else if (choice == 5) {
                         matrix();
+                    } else if (choice == 4) {
+                        multiply(matB, matB);
+                        System.out.printf("|%d %d %d|\n" +
+                                "|%d %d %d|\n" +
+                                "|%d %d %d|\n", matM[0][0], matM[0][1], matM[0][2], matM[1][0], matM[1][1], matM[1][2], matM[2][0], matM[2][1], matM[2][2]);
                     } else {
                         System.out.println("Invalid choice");
                     }
                 }
             } else if (choice == 3) {
-                multiply();
+                multiply(matA, matB);
                 System.out.printf("|%d %d %d|\n" +
                         "|%d %d %d|\n" +
                         "|%d %d %d|\n", matM[0][0], matM[0][1], matM[0][2], matM[1][0], matM[1][1], matM[1][2], matM[2][0], matM[2][1], matM[2][2]);
@@ -111,16 +121,16 @@ public class Matrix {
         }
     }
 
-    public static void multiply() {
-        matM[0][0] = matA[0][0] * matB[0][0] + matA[0][1] * matB[1][0] + matA[0][2] * matB[2][0];
-        matM[0][1] = matA[0][0] * matB[0][1] + matA[0][1] * matB[1][1] + matA[0][2] * matB[2][1];
-        matM[0][2] = matA[0][0] * matB[0][2] + matA[0][1] * matB[1][2] + matA[0][2] * matB[2][2];
-        matM[1][0] = matA[1][0] * matB[0][0] + matA[1][1] * matB[1][0] + matA[1][2] * matB[2][0];
-        matM[1][1] = matA[1][0] * matB[0][1] + matA[1][1] * matB[1][1] + matA[1][2] * matB[2][1];
-        matM[1][2] = matA[1][0] * matB[0][2] + matA[1][1] * matB[1][2] + matA[1][2] * matB[2][2];
-        matM[2][0] = matA[2][0] * matB[0][0] + matA[2][1] * matB[1][0] + matA[2][2] * matB[2][0];
-        matM[2][1] = matA[2][0] * matB[0][1] + matA[2][1] * matB[1][1] + matA[2][2] * matB[2][1];
-        matM[2][2] = matA[2][0] * matB[0][2] + matA[2][1] * matB[1][2] + matA[2][2] * matB[2][2];
+    public static void multiply(int[][] mat1, int[][] mat2) {
+        matM[0][0] = mat1[0][0] * mat2[0][0] + mat1[0][1] * mat2[1][0] + mat1[0][2] * mat2[2][0];
+        matM[0][1] = mat1[0][0] * mat2[0][1] + mat1[0][1] * mat2[1][1] + mat1[0][2] * mat2[2][1];
+        matM[0][2] = mat1[0][0] * mat2[0][2] + mat1[0][1] * mat2[1][2] + mat1[0][2] * mat2[2][2];
+        matM[1][0] = mat1[1][0] * mat2[0][0] + mat1[1][1] * mat2[1][0] + mat1[1][2] * mat2[2][0];
+        matM[1][1] = mat1[1][0] * mat2[0][1] + mat1[1][1] * mat2[1][1] + mat1[1][2] * mat2[2][1];
+        matM[1][2] = mat1[1][0] * mat2[0][2] + mat1[1][1] * mat2[1][2] + mat1[1][2] * mat2[2][2];
+        matM[2][0] = mat1[2][0] * mat2[0][0] + mat1[2][1] * mat2[1][0] + mat1[2][2] * mat2[2][0];
+        matM[2][1] = mat1[2][0] * mat2[0][1] + mat1[2][1] * mat2[1][1] + mat1[2][2] * mat2[2][1];
+        matM[2][2] = mat1[2][0] * mat2[0][2] + mat1[2][1] * mat2[1][2] + mat1[2][2] * mat2[2][2];
     }
 
     public static void inverse() {
